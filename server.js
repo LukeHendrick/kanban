6113d4647e835699bb98b91106e30cc6f9902620
Luke Hendrick <lukehendrick41@gmail.com>
2017-10-13 16:12:42 -0500

    Added HTTP => HTTPS redirect

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const compression = require('compression')
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use('*', (req, res, next) => {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+        return res.redirect('https://' + req.headers.host + req.url)
+    } else {
+        return next();
+    }
+})
+
+
 app.use(compression());
 app.use(express.static(__dirname + '/dist'));
 
