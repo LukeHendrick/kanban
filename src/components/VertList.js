36c8d75f9f50b8a678a0a29ca6a7194b11999def
Luke Hendrick <lukehendrick41@gmail.com>
2018-09-27 22:24:56 -0500

    Updated lanes in an attempt to scale with screen size

--- src/components/VertList.js
+++ src/components/VertList.js
@@ -5,9 +5,9 @@ import VertItem from './VertItem';
 import TitleMenu from './menu/TitleMenu';
 
 const grid = 8;
-const getListStyle = (isDraggingOver, count) => ({
+const getListStyle = (isDraggingOver, count, length) => ({
   background: isDraggingOver ? 'rgba(173, 216, 230, 0.6)' : 'transparent',
-  height: count >= 5 ? 'auto' : '40rem',
+  height: count >= 5 ? 'auto' : length,
   zIndex: '-2',
 });
 
@@ -22,58 +22,62 @@ const getContainerStyle = () => ({
   boxShadow: '2px 3px 10px 4px rgba(255,255,255,0.4)',
 });
 
-const VertList = props => (
-  <Draggable draggableId={`drag${props.title}`} index={props.index}>
-    {provided => (
-      <div>
-        <ContextMenuTrigger id={`listcontext${props.title}`}>
-          <div
-            style={{ width: '2rem' }}
-            ref={provided.innerRef}
-            {...provided.draggableProps}
-            {...provided.dragHandleProps}
-          >
-            <div style={getContainerStyle()}>
-              <div>
-                <h2>{props.title}</h2>
+const VertList = (props) => {
+  const length = screen.height > 900 ? '40rem' : '30rem';
+  return (
+    <Draggable draggableId={`drag${props.title}`} index={props.index}>
+      {provided => (
+        <div>
+          <ContextMenuTrigger id={`listcontext${props.title}`}>
+            <div
+              style={{ width: '2rem' }}
+              ref={provided.innerRef}
+              {...provided.draggableProps}
+              {...provided.dragHandleProps}
+            >
+              <div style={getContainerStyle()}>
+                <div>
+                  <h2>{props.title}</h2>
+                </div>
+                <Droppable type="LIST" droppableId={props.title}>
+                  {(dropProvided, dropSnapshot) => (
+                    <div
+                      ref={dropProvided.innerRef}
+                      style={getListStyle(dropSnapshot.isDraggingOver, props.items.length, length)}
+                    >
+                      {props.items.map((item, index) => (
+                        <VertItem
+                          item={item}
+                          itemMoving={props.itemMoving}
+                          draggingOver={dropSnapshot.isDraggingOver}
+                          color={item.color}
+                          column={props.title}
+                          last={props.last}
+                          index={index}
+                          key={item.id}
+                          deleteStart={props.deleteStart}
+                          editStart={props.editStart}
+                        />
+                      ))}
+                      <div>{dropProvided.placeholder}</div>
+                    </div>
+                  )}
+                </Droppable>
               </div>
-              <Droppable type="LIST" droppableId={props.title}>
-                {(dropProvided, dropSnapshot) => (
-                  <div
-                    ref={dropProvided.innerRef}
-                    style={getListStyle(dropSnapshot.isDraggingOver, props.items.length)}
-                  >
-                    {props.items.map((item, index) => (
-                      <VertItem
-                        item={item}
-                        itemMoving={props.itemMoving}
-                        draggingOver={dropSnapshot.isDraggingOver}
-                        color={item.color}
-                        column={props.title}
-                        last={props.last}
-                        index={index}
-                        key={item.id}
-                        deleteStart={props.deleteStart}
-                        editStart={props.editStart}
-                      />
-                    ))}
-                    <div>{dropProvided.placeholder}</div>
-                  </div>
-                )}
-              </Droppable>
             </div>
-          </div>
-          {provided.placeholder}
-        </ContextMenuTrigger>
-        <TitleMenu
-          id={`listcontext${props.title}`}
-          type="list"
-          editStart={props.editStart}
-          deleteStart={props.deleteStart}
-          index={props.index}
-        />
-      </div>
-    )}
-  </Draggable>
-);
+            {provided.placeholder}
+          </ContextMenuTrigger>
+          <TitleMenu
+            id={`listcontext${props.title}`}
+            type="list"
+            editStart={props.editStart}
+            deleteStart={props.deleteStart}
+            index={props.index}
+          />
+        </div>
+      )}
+    </Draggable>
+  );
+};
+
 export default VertList;
